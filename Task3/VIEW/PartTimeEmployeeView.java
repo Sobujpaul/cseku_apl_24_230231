b762415718c0ef5cdda4c5b9829c7ddb35e6eb30
Sobuj Chandra Paul <146801317+Sobujpaul@users.noreply.github.com>
2024-12-05 13:36:00 +0600

    Add files via upload

---
+++ Task3/VIEW/PartTimeEmployeeView.java
@@ -0,0 +1,7 @@
+package VIEW;
+import MODEL.PartTimeEmployee;
+public class PartTimeEmployeeView {
+    public void displayPartTimeEmployeeDetails(PartTimeEmployee employee) {
+    }  // see which employee is fulTime and PartTime
+}
+
---
+++ Task3/VIEW/PayrollView.java
@@ -0,0 +1,8 @@
+package VIEW;
+
+public class PayrollView {
+    // Method to display the salary of an employee
+    public void displaySalary(double salary) {
+    }
+}
+
